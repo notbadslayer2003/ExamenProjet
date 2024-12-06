1de0a3ee0e40ebd752c6d26a88a608f79895e0dc
Mathis <mathisbury@hotmail.fr>
2024-12-06 08:10:46 +0100

    Added Swagger Config

--- src/main/java/com/stuttgartspeed/backend/DemoConfiguration.java
+++ src/main/java/com/stuttgartspeed/backend/DemoConfiguration.java
@@ -6,7 +6,6 @@ import com.stuttgartspeed.backend.service.CarUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 @Configuration
 public class DemoConfiguration
 {
