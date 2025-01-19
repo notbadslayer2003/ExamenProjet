8312fad694baed50c5169faaf518a3ff307e40bb
Mathis Bury <mathisbury@hotmail.fr>
2025-01-19 17:30:55 +0100

    feat: added CORS configuration

--- src/main/java/com/stuttgartspeed/backend/config/SecurityConfig.java
+++ src/main/java/com/stuttgartspeed/backend/config/SecurityConfig.java
@@ -9,7 +9,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 
 
 @Configuration
@@ -41,6 +45,20 @@ public class SecurityConfig{
         JwtAuthenticationConverter grantedAuthoritiesConverter = new JwtAuthenticationConverter();
         grantedAuthoritiesConverter.setJwtGrantedAuthoritiesConverter(keycloakAccessConverter);
         return grantedAuthoritiesConverter;
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("https://stuttgartspeed-fe.onrender.com"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 
 }
